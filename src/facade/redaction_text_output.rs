4b3f86da899b65ed08065c1afebb0b4168cb28d8
Haixing Hu <starfish.hu@gmail.com>
2026-08-22 13:09:17 +0800

    feat: serialize redaction text outputs

--- src/facade/redaction_text_output.rs
+++ src/facade/redaction_text_output.rs
@@ -48,3 +48,13 @@ impl RedactionTextOutput {
         (self.text, self.summary)
     }
 }
+
+#[cfg(feature = "serde")]
+impl serde::Serialize for RedactionTextOutput {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.text.as_str())
+    }
+}
